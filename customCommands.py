7a90a3b9a5b25edc2f20f53893a3e725fa7aa2ec
BillPlayzToday <tonileoels@gmail.com>
2022-05-09 17:33:29 +0200

    Removed Single Request Keys
    Now directly requested from "connector.py/keyList". (Line 17-22)

--- customCommands.py
+++ customCommands.py
@@ -3,23 +3,11 @@ import connector as Connector
 
 
 commands = {
-    # Chains
     "TurnOff": (2,["Power"]),
     "AV-Mute-Flicker": (2,["AV-Mute"]),
     "Source-Flicker": (2,["HDMI2","HDMI1"]),
     "MaxVolume": (15,["VolumeUp"]),
-    "MinVolume": (15,["VolumeDown"]),
-    # Singles
-    "SourceSearch": (1,["SourceSearch"]),
-    "Freeze": (1,["Freeze"]),
-    "HDMI1": (1,["HDMI1"]),
-    "HDMI2": (1,["HDMI2"]),
-    "Computer1": (1,["Computer1"]),
-    "Computer2": (1,["Computer2"]),
-    "Video": (1,["Video"]),
-    "USB-Display": (1,["USB-Display"]),
-    "USB": (1,["USB"]),
-    "LAN": (1,["LAN"])
+    "MinVolume": (15,["VolumeDown"])
 }
 
 
